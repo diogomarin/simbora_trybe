bdf84284c123df001dec95b0b42fd269764328f3
diogomarin <diogoamarin@gmail.com>
2021-03-12 09:24:22 -0300

    Review of exercise 7

--- exercises/bloco_4/4.4/ex7_maior_indice.js
+++ exercises/bloco_4/4.4/ex7_maior_indice.js
@@ -11,3 +11,17 @@ function maxNumberIndex (array) {
 }
 
 console.log(maxNumberIndex([2, 3, 6, 7, 10, 1]));
+
+
+
+function highestCount(numbers) {
+  let maxNumber = 0;
+  for(let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > maxNumber) {
+      maxNumber = numbers[index];
+    }
+  }
+  return maxNumber
+}
+
+  console.log(highestCount([5, 20, 30, 20, 10, 10])); 
